5712809026a0a9eb5e55267e744a40e581d40492
SusanaEstrada <platazul_12@hotmail.com>
2019-02-26 10:04:53 -0600

    creacion currencies.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { currencies1 } from './currencies/currencies.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    currencies1
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,currencies1]
 })
 export class AppModule { }
